92a6481626e4fff6fcedb7d109c253f69e07c6ab
wovert <67668283@qq.com>
2018-12-06 10:01:05 +0800

    format 100 chickent algrithm

--- C/source/100_chicken.c
+++ C/source/100_chicken.c
@@ -10,8 +10,10 @@
 
 #include <stdio.h>
 
+/**
+ * 这个是最笨的方法，根据cock、hen、chick可能的组合全部遍历，看哪些符合方程条件，运算步骤上万步
+ */
 void algrithm1() {
-	// 这个是最笨的方法，根据cock、hen、chick可能的组合全部遍历，看哪些符合方程条件，运算步骤上万步。
 
 	int cock, hen, chick, count;						// 定义变量公鸡，母鸡，小鸡
 	for(cock=0; cock<=20; cock++) { // 公鸡可能的数量遍历 
@@ -26,9 +28,10 @@ void algrithm1() {
 	printf("algrithm1 loop %d\n\n", count); 
 }
 
+/**
+ * 	算法二:我们可以通过Y=100-X-Y这条已知条件，减去一个循环，使算法复杂法降为N的平方，运算步骤减至几百步
+ */
 void algrithm2() {
-
-	// 算法二:我们可以通过Y=100-X-Y这条已知条件，减去一个循环，使算法复杂法降为N的平方，运算步骤减至几百步。
 	int cock,hen,chick,count=0; //定义变量公鸡，母鸡，小鸡
 	for(cock=0;cock<=20;cock++) {//公鸡可能的数量遍历 
 		for(chick=0;chick<=100-cock;chick=chick+3) //小鸡数是3的倍数且不可能大于100-公鸡数 
@@ -43,9 +46,12 @@ void algrithm2() {
 	printf("algrithm2 loop %d\n\n", count); 	
 }
 
-void algrithm3() {
-	// 设法一个循环解决问题，通过5cock+3hen+chick/3=100得出15cock+9hen+chick=300,再因chick=100-cock-hen,得出15cock+9hen+(100-cock-hen)=300,进而得出14cock+8hen=200再得出7cock+4hen=100(x和y都是整数）。通过先用数学方法的简化，运算步骤减至几十步。
 
+/**
+ * 设法一个循环解决问题，通过5cock+3hen+chick/3=100得出15cock+9hen+chick=300,再因chick=100-cock-hen,得出15cock+9hen+(100-cock-hen)=300,进而得出14cock+8hen=200再得出7cock+4hen=100(x和y都是整数）。通过先用数学方法的简化，运算步骤减至几十步
+ */
+void algrithm3() {
+	
 	int cock,hen,chick;//定义变量公鸡，母鸡，小鸡
 	int count = 0;
 	for(cock=0;cock<15;cock++) { //根据7cock+4hen=100,得出cock小于15 
@@ -69,7 +75,6 @@ int main(void) {
 	algrithm2();
 
 	algrithm3();
-
 
 	return 0;
 }
